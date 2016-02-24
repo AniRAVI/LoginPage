d30e29807ee51da4de1e5e5ea897cfb4c0da39f0
anitha <anitha@ANITRAVI.in.ibm.com>
2016-02-24 13:25:42 +0530

    Initial Commit For Login Details

---
+++ src/com/login/LoginPage.java
@@ -0,0 +1,16 @@
+package com.login;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginPage extends HttpServlet{
+	
+	public void Service(HttpServletRequest req, HttpServletResponse res){
+		
+		
+		
+		
+	}
+
+}
